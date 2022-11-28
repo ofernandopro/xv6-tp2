f3773493abbeeff4cc148739154360afe9d00149
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-11-28 20:11:30 -0300

    salvando

--- proc.c
+++ proc.c
@@ -345,6 +345,7 @@ int lotteryTotal(void) {
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
+/*
 void
 scheduler(void)
 {
@@ -358,6 +359,7 @@ scheduler(void)
     runval++;
     // Enable interrupts on this processor.
     sti();
+    */
 /*
     //int tickets_passed = 0;
     int totalTickets = 0;
@@ -382,6 +384,7 @@ scheduler(void)
     long winner = minTicketsProcess;
     //cprintf("winner -> %d", winner);
 */
+/*
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
 
@@ -423,7 +426,44 @@ scheduler(void)
     release(&ptable.lock);
   }
   }
+}*/
+
+void
+scheduler(void)
+{
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+  
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
+  }
 }
+
 /*
 void
 lottery_scheduler(void)

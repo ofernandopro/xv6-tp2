e98b25275116d86af48472cdaea7a63aae04febf
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-11-29 17:18:10 -0300

    Outra implementaçao

--- sanity.c
+++ sanity.c
@@ -94,10 +94,11 @@ int main(int argc, char* argv[])
     }
 
     // Create children
+    int aux = 10;
     for (i = 0; i < 3 * n; i++)
     {
         int pid = fork();
-        set_tickets(random_at_most(20));
+        set_tickets(aux * (i+1));
 
         if (pid==0)
         {

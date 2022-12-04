29e1a94b175a8bac1862874fe5ddff15fcdd9f2f
Fernando Moreira <fernandoepm.pro@gmail.com>
2022-12-04 20:39:50 -0300

    tentando lottery (FUNCIONANDO)

--- proc.c
+++ proc.c
@@ -355,7 +355,7 @@ scheduler(void)
 {
   struct proc *p;
   struct cpu *c = mycpu();
-  //c->proc = 0;
+  c->proc = 0;
   int totalTickets, runval = 0;
   int winnerTicket;
   
@@ -413,6 +413,45 @@ scheduler(void)
   }
   
 }
+/*
+void
+scheduler(void)
+{
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+  
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
+
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
+
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
+    release(&ptable.lock);
+
+  }
+}*/
+
+
+
 
 int getWinnerProc(void)
 {
